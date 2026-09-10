2d83ddd7270bf168e737819d2edc25549f2affc2
JAYJAYTGG <155000328+JJTGG@users.noreply.github.com>
2026-09-10 11:01:45 +0100

    style: add project fonts

---
+++ app/fonts.ts
@@ -0,0 +1,11 @@
+import { Fraunces, Inter } from "next/font/google";
+
+export const fraunces = Fraunces({
+  subsets: ["latin"],
+  variable: "--font-fraunces",
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
