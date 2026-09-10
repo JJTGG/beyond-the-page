f5f6f6b3c4b70dfcc253b909a75df9724782a2d2
JAYJAYTGG <155000328+JJTGG@users.noreply.github.com>
2026-09-10 11:07:36 +0100

    feat: render page shell on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
+import PageShell from "./components/PageShell";
+
 export default function Home() {
-  return (
-    <main>
-      <h1>Beyond the Page</h1>
-      <p>My Youth Power in Action Journey.</p>
-    </main>
-  );
+  return <PageShell />;
 }
